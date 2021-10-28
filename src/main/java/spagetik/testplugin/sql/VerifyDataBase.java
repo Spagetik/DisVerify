12d6bd3d9720899158d9f94fd81a8260c9be0cf4
Spagetik <lazo12kirill@gmail.com>
2021-10-28 19:23:48 +0300

    Some changes

--- src/main/java/spagetik/testplugin/sql/VerifyDataBase.java
+++ src/main/java/spagetik/testplugin/sql/VerifyDataBase.java
@@ -11,15 +11,15 @@ public class VerifyDataBase extends DataBase {
 
     public boolean addCodeToDb (UUID uuid, int code) {
         String[] data = new String[2];
-        data[0] = "" + uuid;
-        data[1] = "" + code;
+        data[0] = String.valueOf(uuid);
+        data[1] = String.valueOf(code);
         this.SendSqlRequest("INSERT INTO verify_codes (uuid, code) VALUES (?, ?)", data);
         return true;
     }
 
     public boolean removeCodeFromDb (int code) {
         String[] del_data = new String[1];
-        del_data[0] = "" + code;
+        del_data[0] = String.valueOf(code);
         this.SendSqlRequest("DELETE FROM verify_codes WHERE code = ?", del_data);
         return true;
     }
